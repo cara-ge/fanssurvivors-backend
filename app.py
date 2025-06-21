a36a5fabb28717fc621ffb4674c8ad70497e7d83
cara-ge <gexiaohong20042004@126.com>
2025-06-22 00:36:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,8 @@ def download_video():
         return jsonify({"status": "success", "filename": os.path.basename(filename)})
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
+
+# ✅ 关键在这里
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
